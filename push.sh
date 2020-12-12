666176d28c23ea105a41f4aa19768dd36492426d
hi-rustin <rustin.liu@gmail.com>
2020-12-12 16:14:49 +0800

    test: refine

--- push.sh
+++ push.sh
@@ -4,4 +4,4 @@ git config --global github.user rustin-bot
 
 git config --global github.token $(cat /etc/github/token)=
 
-git push -u $(git symbolic-ref --short -q HEAD):try -f
+git push -u $(git remote) $(git symbolic-ref --short -q HEAD):try -f
