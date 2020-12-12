300fe4c62a8888bd49cd107b76c8eb2482e7edee
hi-rustin <rustin.liu@gmail.com>
2020-12-12 16:02:40 +0800

    test: refine

--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-echo $(git remote)
-git remote add origin "https://github.com/tidb-community-bots/test-dev"
-git push origin $(git symbolic-ref --short -q HEAD):try -f
+
+git remote add test "git@github.com:{tidb-community-bots}/{test-dev}.git"
+
+git push test $(git symbolic-ref --short -q HEAD):try -f
