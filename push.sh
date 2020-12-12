a26020f329ef6a736113025d81808bc6fdb33098
hi-rustin <rustin.liu@gmail.com>
2020-12-12 16:23:38 +0800

    test: refine

--- push.sh
+++ push.sh
@@ -4,6 +4,6 @@ git config --global github.user rustin-bot
 
 git config --global github.token $(cat /etc/github/token)
 
-git remote add origin "git@github.com:{tidb-community-bots}/{test-dev}.git"
+git remote add test "git@github.com:{tidb-community-bots}/{test-dev}.git"
 
-git push --set-upstream master:try master -f
+git push --set-upstream master:try test -f
