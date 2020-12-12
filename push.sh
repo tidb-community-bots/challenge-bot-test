6cfaf49b945dd3a7d19eaef3610c144d66f87761
hi-rustin <rustin.liu@gmail.com>
2020-12-12 16:10:27 +0800

    test: refine

--- push.sh
+++ push.sh
@@ -4,6 +4,4 @@ git config --global github.user rustin-bot
 
 git config --global github.token $(cat /etc/github/token)
 
-git remote add test "https://github.com/tidb-community-bots/test-dev"
-
-git push test $(git symbolic-ref --short -q HEAD):try -f
+git push $(git symbolic-ref --short -q HEAD):try -f
